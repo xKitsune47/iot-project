e17605937d5a5b3d7b41dfede8528f6cea179743
Franciszek Cybruch <franek.cy@wp.pl>
2024-04-26 16:40:14 +0200

    added time change between measurements

--- readSensor.py
+++ readSensor.py
@@ -7,9 +7,15 @@ sensor = adafruit_dht.DHT11(board.D20)
 
 last_temperature = 0
 last_humidity = 0
-
+save_data = open("iot_share/temp-moist-data.txt", "a")
 while True:
-	save_data = open("iot_share/temp-moist-data.txt","a")
+	settings_file = open("iot_share/settings.txt", "r")
+	lines = settings_file.readlines()
+	wait_time = float(lines[0].split(' ')[1])
+	if wait_time < 1:
+		print("Time between measurements cannot be lower than 1 second")
+		exit()
+
 	now = datetime.datetime.now().replace(microsecond=0).isoformat()
 	try:
 		temperature = last_temperature = sensor.temperature
@@ -17,6 +23,8 @@ while True:
 		print(f"{now}: {temperature}°C, {humidity}%")
 	except RuntimeError:
 		print(f"{now}: {temperature}°C, {humidity}%")
+
 	save_data.write(f"{now},{last_temperature}-{last_humidity}\n")
-	save_data.close()
-	time.sleep(1)
+	save_data.flush()
+	settings_file.close()
+	time.sleep(wait_time)
